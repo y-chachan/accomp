edf41542ce403bde03a69d431caa81452f24b788
y-chachan <yayaatichachan@gmail.com>
2026-06-16 11:01:42 -0700

    reorganized init, dealt with missing refractory measurement, added a function to estimate fraction of an element in solids

--- accomp/class_Planet.py
+++ accomp/class_Planet.py
@@ -1,8 +1,9 @@
 import numpy as np
+import textwrap
 from .utils import get_mass_fraction
 
 class Planet:
-    def __init__(self, star, measured_abund_dict):
+    def __init__(self, star, measured_abund_dict, loc=None):
         """
         Creates an instance of Planet object.
 
@@ -10,19 +11,24 @@ class Planet:
         ----------
         star_object: input an instance of class_Star for stellar properties. The stellar composition assigned to this object must correspond to the values relative to which the abundance dictionary for the planet is provided.
         measured_abund_dict: must be a dictionary containing abundance of different elements with respect to H normalized to stellar values. The keys of the dictionary must correspond to the different elements that have been measured.
+        loc: float specifying the location of a planet in au
         """
 
         self.star = star
         self.measured_abund = measured_abund_dict
+        self.loc = loc
+
         self.species_name = self.measured_abund.keys()
-        self.abund_dict = self.star.get_basic_abund_dict()
-
-        for sp in self.species_name:
-            self.abund_dict[sp] = star.abundance_dict[sp] + np.log10(self.measured_abund[sp]) 
-
+        self.set_abundance_dict()
         self.mass_averaged_enrichment = self.get_mass_averaged_enrichment(self.measured_abund)
         self.mass_fraction = get_mass_fraction(self.abund_dict) #X, Y, Z
 
+    def set_abundance_dict(self):
+        """Set up an abundance dictionary with log n_H = 12 and additional elements that have been measured."""
+        self.abund_dict = self.star.get_basic_abund_dict()
+
+        for sp in self.species_name:
+            self.abund_dict[sp] = self.star.abundance_dict[sp] + np.log10(self.measured_abund[sp]) 
 
     def set_planet_mass(self, mass, mass_err=None):
         """     
@@ -45,7 +51,7 @@ class Planet:
         self.metal_mass = self.mass_fraction[2] * self.mass
 
     def get_mass_averaged_enrichment(self, enrichment_dict):
-        """Returns the absolute amount of metals in the planet. Same unit as planet mass. Set planet mass before calling this function"""
+        """Returns the mass fraction averaged enrichment of elements in the planet."""
 
         mass_averaged_enrichment = 0.
         for sp in enrichment_dict.keys():
@@ -58,6 +64,22 @@ class Planet:
 
         return self.get_mass_averaged_enrichment(component_enrichment_dict) / self.mass_averaged_enrichment * self.mass_fraction[2]
 
+
+    @staticmethod
+    def simulate_refractory_measurement(measured_abund, refractory_list, solids_comp):
+        """For a given composition of disk solids, take the element with largest fraction in solids amongst the measured ones and use it to calculate the enrichment we would expect for a fully refractory species. """
+
+        measured_elem_solids_comp = {key: solids_comp[key] for key in measured_abund.keys()}
+        most_ref_element = max(measured_elem_solids_comp, key=measured_elem_solids_comp.get)
+
+        simulated_measurements = {}
+        for ref in refractory_list:
+            if ref in measured_abund.keys():
+                print('Element ' + ref + ' already amongst measured species.')
+                continue
+            simulated_measurements[ref] = (measured_abund[most_ref_element] - (1 - solids_comp[most_ref_element])) / solids_comp[most_ref_element]
+        return simulated_measurements
+
     def get_metal_mass_breakdown(self, solids_comp, gas_comp, main_ref_elem):
         """Pass in a dictionary of the solid and gas composition and obtain the metal mass accreted via local solids and gas, and excess metals that are unnacounted for by local material.
     
@@ -68,20 +90,24 @@ class Planet:
         main_ref_elem: The element amongst the measured ones that will constitute the refractory reference. 
         """
 
-        self.enrichment_from_solid = self.star.get_basic_abund_dict()
-        self.enrichment_from_local_gas = self.star.get_basic_abund_dict()
-        self.enrichment_from_excess_metal = self.star.get_basic_abund_dict()
-
+        self.enrichment_from_solid = {}
+        self.enrichment_from_local_gas = {}
+        self.enrichment_from_excess_metal = {}
+        
         #The following equations assume this.
-        assert(solids_comp[main_ref_elem] == 1.), print('The reference refractory element must be present entirely in solids.')
+        assert(solids_comp[main_ref_elem] == 1.), print(textwrap.fill('The reference refractory element must be present entirely in solids. If no such element exists amongst the measured ones, use method simulate_refractory_measurement to simulate measurements for a fully refractory species and add it to planet measurements'))
 
         for elem in self.species_name:
             if solids_comp[elem] == 1.:
                 self.enrichment_from_solid[elem] = self.measured_abund[elem]
+                self.enrichment_from_local_gas[elem] = 0.
             else:
                 self.enrichment_from_solid[elem] = self.measured_abund[main_ref_elem] * solids_comp[elem]
                 self.enrichment_from_local_gas[elem] = gas_comp[elem]
-                self.enrichment_from_excess_metal[elem] = np.maximum((self.measured_abund[elem] - 1) - (self.measured_abund[main_ref_elem] - 1) * solids_comp[elem], 0.)
+                self.enrichment_from_excess_metal[elem] = (self.measured_abund[elem] - 1) - (self.measured_abund[main_ref_elem] - 1) * solids_comp[elem] #np.maximum((self.measured_abund[elem] - 1) - (self.measured_abund[main_ref_elem] - 1) * solids_comp[elem], 0.)
+
+            if np.any(self.enrichment_from_local_gas[elem] + self.enrichment_from_solid[elem] > self.measured_abund[elem]):
+                print(textwrap.fill('For a fraction of samples, the predicted enrichment for element ' + elem + ' is greater than the measured enrichment due to the assumed split for this element between local solid and gas composition. This is expected because planet measurements can \'fall\' on both sides of the predicted composition. Use function constrain_elem_frac_in_solids to put empirical constraints on the split of elements between disk solids and gas.'))
 
         self.mass_fraction_from_solids = self.get_component_mass_fraction(self.enrichment_from_solid)
         self.mass_fraction_from_local_gas = self.get_component_mass_fraction(self.enrichment_from_local_gas)
@@ -93,5 +119,22 @@ class Planet:
         self.metal_mass_local_gas = self.mass_fraction_from_local_gas * self.mass
         self.metal_mass_excess_metal = self.mass_fraction_from_excess_metal * self.mass
 
+    def constrain_elem_frac_in_solids(self, main_ref_elem):
+        """Takes the measured abundances to estimate the fraction of each element that must be present in the solids such that measured abundance is a result of accretion of local solids and gas.
+    
+        Parameters
+        ----------   
+        main_ref_elem: The element amongst the measured ones that will constitute the refractory reference. 
+        """
 
-    #TODO: write module to compare planetary measurements with expectations
+        #TODO: add this to example
+
+        f_v_dict = {}
+
+        for elem in self.species_name:
+            #if np.all(self.measured_abund[elem] / self.measured_abund[main_ref_elem] == 1.):
+            # if elem == main_ref_elem:
+            #     continue
+            f_v_dict[elem] = (self.measured_abund[elem] - 1.) / (self.measured_abund[main_ref_elem] - 1.)
+
+        return f_v_dict
