c0b44037d569761fb23697eca65bac461bf7701d
y-chachan <yayaatichachan@gmail.com>
2026-06-16 10:58:12 -0700

    updated condensation temperatures of species

--- accomp/class_Molecule.py
+++ accomp/class_Molecule.py
@@ -148,18 +148,21 @@ class MoleculeDict():
         self.molecule_dict['CO'] = Molecule('CO', 30.)
         self.molecule_dict['CO'].set_elem_fraction(self.star, 'C', 0.4)
 
-        self.molecule_dict['CO2'] = Molecule('CO2', 70.)
+        self.molecule_dict['CO2'] = Molecule('CO2', 70.) #Fray & Schmitt 2009
         self.molecule_dict['CO2'].set_elem_fraction(self.star, 'C', 0.1)
 
         #N-bearing species
         self.molecule_dict['N2'] = Molecule('N2', 25.)
         self.molecule_dict['N2'].set_elem_fraction(self.star, 'N', 0.6)
 
-        self.molecule_dict['NH4SH'] = Molecule('NH4SH', 300.) #check temperature
+        self.molecule_dict['NH4SH'] = Molecule('NH4SH', 200.) #ammonium salts have slightly higher Tcond than water
         self.molecule_dict['NH4SH'].set_elem_fraction(self.star, 'N', 0.15)
 
-        self.molecule_dict['NH3'] = Molecule('NH3', 70.) #check temperature
-        self.molecule_dict['NH3'].set_elem_fraction(self.star, 'N', 0.25)
+        self.molecule_dict['NH4'] = Molecule('NH4', 200.) #ammonium salts have slightly higher Tcond than water
+        self.molecule_dict['NH4'].set_elem_fraction(self.star, 'N', 0.15)
+
+        self.molecule_dict['NH3'] = Molecule('NH3', 110.) #Fray & Schmitt 2009
+        self.molecule_dict['NH3'].set_elem_fraction(self.star, 'N', 0.1)
 
         #Nobel gases
         for k in ['Ne', 'Ar']:
@@ -167,23 +170,23 @@ class MoleculeDict():
             self.molecule_dict[k].set_elem_fraction(self.star, k, 1.)
 
         #O-bearing species
-        self.molecule_dict['H2O'] = Molecule('H2O', 170.)
+        self.molecule_dict['H2O'] = Molecule('H2O', 170.) #Fray & Schmitt 2009
         self.molecule_dict['H2O'].set_elem_fraction(self.star, 'O', 0.45)
 
         #S-bearing species
-        self.molecule_dict['H2S'] = Molecule('H2S', 90.)
+        self.molecule_dict['H2S'] = Molecule('H2S', 60.) #Fray & Schmitt 2009
         self.molecule_dict['H2S'].set_elem_fraction(self.star, 'S', 0.1)
 
         #refractories
-        self.molecule_dict['Na'] = Molecule('Na', 1100.)
+        self.molecule_dict['Na'] = Molecule('Na', 1000.) #Lodders 2003
         self.molecule_dict['Na'].set_elem_fraction(self.star, 'Na', 1.)
 
         for k in ['Si', 'Mg', 'Fe', 'Ni']:
-            self.molecule_dict[k] = Molecule(k, 1600.)
+            self.molecule_dict[k] = Molecule(k, 1350.) #Lodders 2003
             self.molecule_dict[k].set_elem_fraction(self.star, k, 1.)
 
         for k in ['Al', 'Ca']:
-            self.molecule_dict[k] = Molecule(k, 2000.)
+            self.molecule_dict[k] = Molecule(k, 1600.) #Lodders 2003
             self.molecule_dict[k].set_elem_fraction(self.star, k, 1.)
 
         #catch remaining fraction - put it in unknown refractory species
@@ -198,14 +201,14 @@ class MoleculeDict():
             if (frac < 1.):
                 print('Putting remaining element fraction for ' + sp + ' in refractory')
                 if sp == 'O':
-                    self.molecule_dict['O_refractory'] = Molecule('O', 1600.)
+                    self.molecule_dict['O_refractory'] = Molecule('O', 1350.) #Lodders 2003
                     self.molecule_dict['O_refractory'].set_elem_fraction(self.star, 'O', 1. - summed_abundances['O'])
                 elif sp == 'C':
                     self.molecule_dict['C_refactory'] = Molecule('C', 300.)
                     self.molecule_dict['C_refactory'].set_elem_fraction(self.star, 'C', 1. - summed_abundances['C'])
                 elif sp == 'S':
                     #Refractory S in some form other than ammonium-sulfate salts
-                    self.molecule_dict['S_refactory'] = Molecule('S', 300.)
+                    self.molecule_dict['S_refactory'] = Molecule('S', 700.) #Lodders 2003, FeS
                     self.molecule_dict['S_refactory'].set_elem_fraction(self.star, 'S', 1. - summed_abundances['S'])
                 elif sp == 'N':
                     #Refractory N in some form other than ammonium-sulfate salts
